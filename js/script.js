66d545229873dd5c5172917183bd80625f89cc6b
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-20 17:01:11 +0100

    (project) release vers. 1.0 and start refactory

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+/*
+Ciao ragazzi,
+Esercizio di oggi: Simon Says
+nome repo: js-simon
+Descrizione:
+Visualizzare in pagina 5 numeri casuali ( tra 1 e 100) non duplicati.
+Da lì parte un timer di 30 secondi.
+Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+Buon pomeriggio e buon lavoro !!!! :muscolo:
+*/
+
+
+
+
+
 function getRandUniqNumb(maximum, lenghtNum) {
   const uniNumb = new Set();
   let arrayNumb = [];
@@ -9,22 +29,54 @@ function getRandUniqNumb(maximum, lenghtNum) {
   return arrayNumb;
 }
 
-function sameNumb(array1, array2) {
-  const arrayNumbUserTrue = []
-  array1.forEach((element) => {
-    if (array2.includes(element)) {
-      arrayNumbUserTrue.push(element);
-      console.log(sameNumb(numb))
-    }
+
+function PlayGame (numbers) {
+  buttPlay.classList.remove('d-none');
+  buttPlay.addEventListener('click', function () {
+    let userCol = document.getElementById('userCol');
+    let userCol1 = document.getElementById('userCol1');
+    let userCol2 = document.getElementById('userCol2');
+    let userCol3 = document.getElementById('userCol3');
+    let userCol4 = document.getElementById('userCol4');
+    userCol.classList.remove('d-none');
+    userCol1.classList.remove('d-none');
+    userCol2.classList.remove('d-none');
+    userCol3.classList.remove('d-none');
+    userCol4.classList.remove('d-none');
+    buttSub.classList.remove('d-none');
+    buttSub.addEventListener('click', function() {
+      let userNumber =parseInt(document.getElementById('user-n').value);
+      let userNumber1 =parseInt(document.getElementById('user-n1').value);
+      let userNumber2 =parseInt(document.getElementById('user-n2').value);
+      let userNumber3 =parseInt(document.getElementById('user-n3').value);
+      let userNumber4 =parseInt(document.getElementById('user-n4').value);
+      let arrayNumbUser = []
+      arrayNumbUser.push(userNumber);
+      arrayNumbUser.push(userNumber1);
+      arrayNumbUser.push(userNumber2);
+      arrayNumbUser.push(userNumber3);
+      arrayNumbUser.push(userNumber4);
+      console.log(arrayNumbUser);
+      let array = [];
+      for (let i = 0; i <= arrayNumbUser.length; i++) {
+        if (numbers.includes(arrayNumbUser[i])) {
+          array.push(arrayNumbUser[i]);
+          console.log(array);
+          let result = document.getElementById('result');
+          result.innerHTML = 'hai indovinato ' + array;
+        }
+      } 
+    })  
   })
 }
 
-let arrayNumbUser = []
 let buttGen = document.getElementById('btn-gen');
 let buttPlay = document.getElementById('btn-start');
+let buttSub = document.getElementById('btn-submit');
 
 buttGen.addEventListener('click', function () {
   let numbers = getRandUniqNumb(100, 5);
+  console.log(numbers);
   let col1 = document.getElementById('col|');
   let col2 = document.getElementById('col||');
   let col3 = document.getElementById('col|||');
@@ -45,24 +97,8 @@ buttGen.addEventListener('click', function () {
     col3.classList.add('d-none');
     col4.classList.add('d-none');
     col5.classList.add('d-none');
-  }, 30000);
+  }, 1000);
+
+  setTimeout(PlayGame(numbers), 1500);
 
 })
-
-buttPlay.addEventListener('click', function () {
-  let userNumber =parseInt(document.getElementById('user-n').value);
-  let userNumber1 =parseInt(document.getElementById('user-n1').value);
-  let userNumber2 =parseInt(document.getElementById('user-n2').value);
-  let userNumber3 =parseInt(document.getElementById('user-n3').value);
-  let userNumber4 =parseInt(document.getElementById('user-n4').value);
-  arrayNumbUser.push(userNumber);
-  arrayNumbUser.push(userNumber1);
-  arrayNumbUser.push(userNumber2);
-  arrayNumbUser.push(userNumber3);
-  arrayNumbUser.push(userNumber4);
-  console.log((arrayNumbUser));
-})
-
-
-
-
